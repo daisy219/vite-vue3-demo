6da72aa508c89efb604f214ecdc6db9789d070ba
zhaoyang <wb.zhaoyang@WB-ZHAOYANG>
2021-04-25 16:28:45 +0800

    '项目结构配置'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,28 @@
-import {
-  createRouter,
-  createWebHashHistory,
-  RouteRecordRaw
-} from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '@/views/home/index.vue'
 import Vuex from '@/views/vuex/index.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/vuex',
-    name: 'Vuex',
-    component: Vuex
-  },
-  {
-    path: '/axios',
-    name: 'Axios',
-    component: () => import('@/views/axios/index.vue') // 懒加载组件
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/vuex',
+		name: 'Vuex',
+		component: Vuex
+	},
+	{
+		path: '/axios',
+		name: 'Axios',
+		component: () => import('@/views/axios/index.vue') // 懒加载组件
+	}
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+	history: createWebHashHistory(),
+	routes
 })
 
 export default router
